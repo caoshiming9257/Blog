d2504f570bd6fcf2ab1f91c3e76d475a4a87ae93
caoshiming9257 <657281919@qq.com>
2020-11-10 21:24:05 +0800

    blog_添加sql文件_v4

--- src/main/java/com/simon/blog/BlogApplication.java
+++ src/main/java/com/simon/blog/BlogApplication.java
@@ -1,12 +1,8 @@
 package com.simon.blog;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
 
 @SpringBootApplication
 public class BlogApplication {
